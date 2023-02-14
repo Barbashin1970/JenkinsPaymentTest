1c6630f390e3790e935509b625fd50f17488e3f0
Oleg_Barbashin <banksnab@gmail.com>
2023-02-14 13:06:06 +0700

    new commit

---
+++ src/main/java/Praktikum.java
@@ -0,0 +1,16 @@
+public class Praktikum {
+    public static String fio = "Oleg Barbashin";
+
+    public static void main(String[] args) {
+        /*
+             В этом методе заложи логику работы с классом Account.
+             Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
+             и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
+         */
+        Account account = new Account(fio);
+        account.checkNameToEmboss();
+
+
+    }
+
+}
---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,84 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class AccountTest {
+    String name;
+    @Test
+    public void checkNameToEmbossEmptyName() {
+        name = "";
+        Account account = new Account(name);
+        Assert.assertFalse(account.checkNameToEmboss());
+    }
+    @Test
+    public void checkNameToEmbossMaxName() {
+        name = "123456789 123456789";
+        Account account = new Account(name);
+        Assert.assertTrue(account.checkNameToEmboss());
+    }
+
+    @Test
+    public void checkNameToEmbossMinName() {
+        name = "1 3";
+        Account account = new Account(name);
+        Assert.assertTrue(account.checkNameToEmboss());
+    }
+
+    @Test
+    public void checkNameToEmbossLongName() {
+        name = "123456789 1234567890";
+        Account account = new Account(name);
+        Assert.assertFalse(account.checkNameToEmboss());
+    }
+
+    @Test
+    public void checkNameToEmbossShortName() {
+        name = "12";
+        Account account = new Account(name);
+        Assert.assertFalse(account.checkNameToEmboss());
+    }
+
+    @Test
+    public void checkNameToEmbossContainsEmptyInside() {
+        name = "123456789 012345678";
+        Account account = new Account(name);
+        Assert.assertTrue(account.checkNameToEmboss());
+    }
+
+    @Test
+    public void checkNameToEmbossContainsNoEmptyInside() {
+        name = "12345678912345678";
+        Account account = new Account(name);
+        Assert.assertFalse(account.checkNameToEmboss());
+    }
+
+    @Test
+    public void checkNameToEmbossContainsNoEmptyInBegin() {
+        name = "123456789 12345678";
+        Account account = new Account(name);
+        Assert.assertTrue(account.checkNameToEmboss());
+    }
+
+    @Test
+    public void checkNameToEmbossContainsEmptyInBegin() {
+        name = " 12345678 12345678";
+        Account account = new Account(name);
+        Assert.assertFalse(account.checkNameToEmboss());
+    }
+
+    @Test
+    public void checkNameToEmbossContainsNoEmptyEnd() {
+        name = "12345678 12345678";
+        Account account = new Account(name);
+        Assert.assertTrue(account.checkNameToEmboss());
+    }
+
+    @Test
+    public void checkNameToEmbossContainsEmptyEnd() {
+        name = " 1234567 12345678 ";
+        Account account = new Account(name);
+        Assert.assertFalse(account.checkNameToEmboss());
+    }
+
+
+}
